c133cdda77f69cd059d916b110c83ba55f2991a5
Gioh Kim <gi-oh.kim@profitbricks.com>
2016-02-16 13:02:29 +0100

    fix errta

--- hello.asm
+++ hello.asm
@@ -1,7 +1,6 @@
 ; To create executable:
-; nasm -f elf64 first.asm
-; gcc -o first first.o main.c
-;
+; nasm -f elf64 hello.asm
+; gcc -o greet hello.o main.c
 
 segment .data
 
@@ -10,17 +9,14 @@ outmsg db    "Hello, world!", 0
 segment .bss
 
 segment .text
-        global  asm_main		; other modules can call asm_main
+	global  asm_main		; other modules can call asm_main
 	extern printf
 
 asm_main:
-        push rdi
+	push rdi
 
 	mov rdi, outmsg
 	call printf
-		
+	
 	pop rdi
-	leave
-	ret
-
-		
+	ret	
